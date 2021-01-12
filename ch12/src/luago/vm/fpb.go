edc9494ba7f5e63effa74688b888787d74023686
sfl <songfeilong_88@163.com>
2021-01-12 16:31:14 +0800

    [Add]添加12章，实现迭代器

---
+++ ch12/src/luago/vm/fpb.go
@@ -0,0 +1,29 @@
+package vm
+
+//converts an integer to a "floating point byte", represented as
+//(eeeeexxx), where the real value is (1xxx) * 2^(eeeee-1)
+//if eeeee != 0 and (xxx) otherwise
+
+func Int2fb(x int) int {
+	e := 0
+	if x < 8 {
+		return x
+	}
+	for x >= (8 << 4) {
+		x = (x + 0xF) >> 4 /*x=ceil(x/16)*/
+		e += 4
+	}
+	for x >= (8 << 1) { //fine steps
+		x = (x + 1) >> 1 //  x = ceil(x / 2)
+		e++
+	}
+	return ((e + 1) << 3) | (x - 8)
+}
+
+func Fb2int(x int) int {
+	if x < 8 {
+		return x
+	} else {
+		return ((x & 7) + 8) << uint((x>>3)-1)
+	}
+}
